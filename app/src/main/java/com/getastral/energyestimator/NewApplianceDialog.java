6301065cee8745ff0cce3e61c3867083cc6ca224
Samuel Jacob <samueldotj@gmail.com>
2014-11-27 10:30:21 -0800

    Dismiss the Add appliance dialog after a selection.

--- app/src/main/java/com/getastral/energyestimator/NewApplianceDialog.java
+++ app/src/main/java/com/getastral/energyestimator/NewApplianceDialog.java
@@ -47,6 +47,8 @@ public class NewApplianceDialog extends DialogFragment {
                         DeviceListAdapter.getInstance().notifyDataSetChanged();
                     }
                 });
+                //close the dialog
+                dismiss();
 
             }
         });
