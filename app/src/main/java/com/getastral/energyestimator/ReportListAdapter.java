8667aa6bb5863e99c70a9e468d4060374790ed04
Samuel Jacob <samueldotj@gmail.com>
2014-12-13 20:21:47 -0800

    Added one report card to show state's electricity rate.

--- app/src/main/java/com/getastral/energyestimator/ReportListAdapter.java
+++ app/src/main/java/com/getastral/energyestimator/ReportListAdapter.java
@@ -1,12 +1,20 @@
 package com.getastral.energyestimator;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridLayout.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.getastral.energyestimator.DatabaseHelper.ElectricityRates;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.BarData;
@@ -17,6 +25,7 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.utils.Legend;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * List Adapter to hold all the reports and statistics.
@@ -26,7 +35,7 @@ public class ReportListAdapter extends BaseAdapter {
 
     private static ReportListAdapter mInstance = null;
 
-    private int TOTAL_REPORTS = 2;
+    private int TOTAL_REPORTS = 3;
 
     protected ReportListAdapter() {
         // Exists only to defeat instantiation.
@@ -67,6 +76,8 @@ public class ReportListAdapter extends BaseAdapter {
             case 0:
                 return getPieChart(convertView, parent);
             case 1:
+                return getElectricitySlabTable(convertView, parent);
+            case 2:
                 return getBarChart(convertView, parent);
         }
         return null;
@@ -75,7 +86,7 @@ public class ReportListAdapter extends BaseAdapter {
     private View getPieChart(View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater) ApplicationGlobals.getAppContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-            convertView = mInflater.inflate(R.layout.item_piechart, null);
+            convertView = mInflater.inflate(R.layout.item_report_appliance_usage_graph, null);
         }
         setupPieChart(convertView);
         return convertView;
@@ -84,10 +95,112 @@ public class ReportListAdapter extends BaseAdapter {
     private View getBarChart(View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater) ApplicationGlobals.getAppContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-            convertView = mInflater.inflate(R.layout.item_barchart, null);
+            convertView = mInflater.inflate(R.layout.item_report_electricity_slab_graph, null);
         }
         setupBarChart(convertView);
         return convertView;
+    }
+
+    private View getElectricitySlabTable(View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater) ApplicationGlobals.getAppContext().getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.item_report_electricity_slab_table, null);
+        }
+        setupElectricitySlabTable(convertView);
+        return convertView;
+    }
+
+    private void addRowDividerToGrid(GridLayout gridLayout, int row, int columnCount) {
+        Context context = ApplicationGlobals.getAppContext();
+        GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+        param.height = LayoutParams.WRAP_CONTENT;
+        param.width = LayoutParams.MATCH_PARENT;
+        param.rightMargin = 5;
+        param.topMargin = 5;
+        param.rowSpec = GridLayout.spec(row);
+        param.columnSpec = GridLayout.spec(0, columnCount);
+
+        LinearLayout linearLayout = new LinearLayout(context);
+        linearLayout.setMinimumHeight(2);
+        linearLayout.setBackgroundColor(Color.DKGRAY);
+        linearLayout.setLayoutParams(param);
+
+        gridLayout.addView(linearLayout);
+    }
+
+    private void addHeaderToGrid(GridLayout gridLayout, int column, String title, int gravity) {
+        Context context = ApplicationGlobals.getAppContext();
+        GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+        param.height = LayoutParams.WRAP_CONTENT;
+        param.width = LayoutParams.WRAP_CONTENT;
+        param.rightMargin = 5;
+        param.topMargin = 5;
+        param.rowSpec = GridLayout.spec(0);
+        param.columnSpec = GridLayout.spec(column, 1f);
+        param.setGravity(gravity);
+
+        TextView textView = new TextView(context);
+        textView.setTextColor(Color.BLACK);
+        textView.setText(title);
+        textView.setTypeface(null, Typeface.BOLD);
+        textView.setLayoutParams(param);
+
+        gridLayout.addView(textView);
+    }
+
+    private void addContentToGrid(GridLayout gridLayout, int row, int column, String text, int gravity) {
+        Context context = ApplicationGlobals.getAppContext();
+        GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+        param.height = LayoutParams.WRAP_CONTENT;
+        param.width = LayoutParams.WRAP_CONTENT;
+        param.rightMargin = 5;
+        param.topMargin = 5;
+
+        param.rowSpec = GridLayout.spec(row);
+        param.columnSpec = GridLayout.spec(column);
+
+        TextView textView = new TextView(context);
+        textView.setTextColor(Color.BLACK);
+        textView.setText(text);
+        textView.setLayoutParams(param);
+        if (row == 4) {
+            textView.setGravity(Gravity.FILL);
+            textView.setBackgroundColor(Color.LTGRAY);
+        }
+
+        gridLayout.addView(textView);
+    }
+
+
+    private void setupElectricitySlabTable(View convertView) {
+        int COLUMN_COUNT = 5, HEADER_ROW_COUNT = 1;
+        int rowCount;
+        // get a reference for the TableLayout
+        GridLayout gridLayout = (GridLayout)convertView.findViewById(R.id.tab_report_electricity_slab_table);
+
+        gridLayout.removeAllViews();
+
+        List<ElectricityRates> list = DatabaseHelper.getElectricityRateList("Tamil Nadu", "Public");
+
+        gridLayout.setColumnCount(COLUMN_COUNT);
+        rowCount = (list.size() + HEADER_ROW_COUNT) * 2;
+        gridLayout.setRowCount(rowCount);
+
+        addHeaderToGrid(gridLayout, 0, "Condition Start", Gravity.FILL_HORIZONTAL);
+        addHeaderToGrid(gridLayout, 1, "Condition End", Gravity.FILL_HORIZONTAL);
+        addHeaderToGrid(gridLayout, 2, "Start", Gravity.FILL_HORIZONTAL);
+        addHeaderToGrid(gridLayout, 3, "End", Gravity.FILL_HORIZONTAL);
+        addHeaderToGrid(gridLayout, 4, "Rate", Gravity.FILL_HORIZONTAL);
+
+        int row = 1;
+        for(ElectricityRates electricityRate: list) {
+            addContentToGrid(gridLayout, row, 0, "" + electricityRate.conditionUnitsStart, Gravity.FILL_HORIZONTAL);
+            addContentToGrid(gridLayout, row, 1, "" + electricityRate.conditionUnitsEnd, Gravity.FILL_HORIZONTAL);
+            addContentToGrid(gridLayout, row, 2, "" + electricityRate.startUnit, Gravity.FILL_HORIZONTAL);
+            addContentToGrid(gridLayout, row, 3, "" + electricityRate.endUnit, Gravity.FILL_HORIZONTAL);
+            addContentToGrid(gridLayout, row, 4, "" + electricityRate.rate, Gravity.FILL_HORIZONTAL);
+            addRowDividerToGrid(gridLayout, row++, COLUMN_COUNT);
+        }
     }
 
     private void setPieChartData(PieChart chart, PowerConsumptionInfo powerConsumptionInfo) {
